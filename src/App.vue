cd68287d5acf0d756a3b9e1db3b3d45109f7bc72
Luis Pena <lpenapec@gmail.com>
2025-02-16 19:54:42 -0600

    Finished addtransaction component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
     import Header from './components/Header.vue';
     import Balance from './components/Balance.vue';
+    import IncomeExpenses from './components/IncomeExpenses.vue';
+    import AddTransaction from './components/AddTransaction.vue';
     import {ref, computed} from 'vue'
 
     const transactions = ref([
@@ -15,6 +17,23 @@
             return acc+x.amount
         },0)
     })
+
+    const moneyIn = computed(()=>{
+        return transactions.value
+        .filter((x)=>x.amount>0)
+        .reduce((acc, x)=>{
+            return acc+x.amount
+        },0)
+    })
+
+    const moneyOut = computed(()=>{
+        return transactions.value
+        .filter((x)=>x.amount<0)
+        .reduce((acc, x)=>{
+            return acc+x.amount
+        },0)
+    })
+
 </script>
 
 
@@ -22,6 +41,8 @@
     <Header></Header>
     <div class="container">
         <Balance :total="sum"></Balance>
+        <IncomeExpenses :income="moneyIn" :expense="moneyOut"></IncomeExpenses>
+        <AddTransaction></AddTransaction>
     </div>
   
 </template>
---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,37 @@
+<script setup>
+    import {ref, defineEmits} from 'vue'
+    const text = ref('')
+    const amount = ref('')
+
+    const emit = defineEmits([
+        'transactionSubmitted'
+    ])
+
+    const onSubmit = ()=>{
+        const transactionsData = {
+            text: text.value,
+            amount: parseFloat(amount.value),
+        }
+
+        emit('transactionSubmitted', transactionsData)
+
+        text.value = ''
+        amount.value = ''
+    }
+
+</script>
+
+<template>
+    <h3>Add a new Transaction</h3>
+    <form id="form" @submit.prevent="onSubmit">
+    <div class="form-control">
+        <label for="text">Enter Transaction</label>
+        <input type="text" id="text" v-model="text" placeholder="Enter Transaction...">
+    </div>
+    <div class="form-control">
+        <label for="amount">Enter Transaction Amount</label>
+        <input type="text" id="amount" v-model="text" placeholder="Enter Negative Value for Expenses">
+    </div>
+    <button class="btn">Add Transaction</button>
+    </form>
+</template>
